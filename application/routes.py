c32164ea48942fb31f477378fdf9c6b460e4ecd3
Sean Atherton <sean@atho.com.au>
2024-05-18 14:24:58 +1000

    safe_str_cmp: Function to perform a constant-time string comparison using hmac.compare_digest. hash_password: Function to hash a password using werkzeug.security.generate_password_hash. check_password: Function to check a hashed password against a plain text password using werkzeug.security.check_password_hash.

--- application/routes.py
+++ application/routes.py
@@ -3,7 +3,7 @@ from application import app, db
 from application.forms import RegistrationForm, LoginForm
 from application.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-from werkzeug.security import generate_password_hash, check_password_hash
+from application.utils import hash_password, check_password
 
 @app.route("/")
 @app.route("/home")
@@ -17,7 +17,7 @@ def register():
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data).decode('utf-8')
+        hashed_password = hash_password(form.password.data)
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
@@ -32,7 +32,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and check_password_hash(user.password, form.password.data):
+        if user and check_password(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home'))
--- application/utils.py
+++ application/utils.py
@@ -1,5 +1,14 @@
 import hmac
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def safe_str_cmp(a, b):
     """Perform a constant time string comparison."""
     return hmac.compare_digest(a, b)
+
+def hash_password(password):
+    """Hash a password for storing."""
+    return generate_password_hash(password)
+
+def check_password(hashed_password, password):
+    """Verify a stored password against one provided by user."""
+    return check_password_hash(hashed_password, password)
